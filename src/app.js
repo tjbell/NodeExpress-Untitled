b4083ea2cfdd3b6145b20a3db78258da515d0c5f
Ashish7129 <ashish.mcs16.du@gmail.com>
2019-01-23 21:58:51 +0530

    module 2

--- src/app.js
+++ src/app.js
@@ -10,6 +10,25 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.render("index", { title: "Index" }));
+const accountData = fs.readFileSync("src/json/accounts.json", "UTF8");
+const accounts = JSON.parse(accountData);
 
+const userData = fs.readFileSync("src/json/users.json", "UTF8");
+const users = JSON.parse(userData);
+
+app.get("/", (req, res) =>
+  res.render("index", { title: "Account Summary", accounts: accounts })
+);
+
+app.get("/savings", (req, res) =>
+  res.render("account", { account: accounts.savings })
+);
+app.get("/checking", (req, res) =>
+  res.render("account", { account: accounts.checking })
+);
+app.get("/credit", (req, res) =>
+  res.render("account", { account: accounts.credit })
+);
+
+app.get("/profile", (req, res) => res.render("profile", { user: users[0] }));
 app.listen(3000, () => console.log("PS Project Running on 3000!"));
