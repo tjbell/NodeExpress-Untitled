4f8687cd374f95311437dfc6cba3e6a986fd0788
Ashish7129 <ashish.mcs16.du@gmail.com>
2019-01-23 22:46:46 +0530

    module 3 mistakes changes

--- src/app.js
+++ src/app.js
@@ -36,12 +36,12 @@ app.post("/transfer", (req, res) => {
   accounts[req.body.from].balance =
     accounts[req.body.from].balance - req.body.amount;
   accounts[req.body.to].balance =
-    parseInt(accounts[req.body.to].balance) - parseInt(req.body.amount, 10);
+    parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
 
-  const accounttsJSON = JSON.stringify(accounts, null, 4);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(
     path.join(__dirname, "json/accounts.json"),
-    accounttsJSON,
+    accountsJSON,
     "utf8"
   );
   res.render("transfer", { message: "Transfer Completed" });
@@ -54,10 +54,10 @@ app.get("/payment", (req, res) => {
 app.post("/payment", (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount, 10);
-  const accounttsJSON = JSON.stringify(accounts, null, 4);
+  const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(
     path.join(__dirname, "json", "accounts.json"),
-    accounttsJSON,
+    accountsJSON,
     "utf8"
   );
   res.render("payment", {
