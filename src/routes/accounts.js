85de89d2cf3688efab7cc6cafbc6331e1e888411
Ashish7129 <ashish.mcs16.du@gmail.com>
2019-01-23 23:36:39 +0530

    changes in json

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const accounts = require("../data");
+const { accounts } = require("../data");
 
 router.get("/savings", (req, res) =>
   res.render("account", { account: accounts.savings })
