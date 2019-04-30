4536b2e9a75cd6d4236589f3eab40a55fc23d88d
NaveenGolakoti <M1047133@mindtree.com>
2019-04-30 12:33:10 +0530

    spelling mistake
    spelling misatke

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.post('/transfer', (req,res) => {
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
     const accountsJSON=JSON.stringify(accounts,null,4);
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'),accountsJSON,'utf8');
-    res.render('transfer',{message:'Transfer completed'});
+    res.render('transfer',{ message:'Transfer Completed'});
 });
 app.get('/payment', (req,res) => res.render('payment',{account:accounts.credit}));
 app.post('/payment', (req,res) =>{
