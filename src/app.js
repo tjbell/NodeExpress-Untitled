ff67de0cd6198cf9dafde28f39e4d8571205e17a
Zak Hyslin <rapscalan@gmail.com>
2020-12-10 15:24:40 -0800

    read accounts read users update index view

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { json } = require('express');
 
 const app = new express();
 app.set('views', path.join(__dirname,'/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
+const accountData = fs.readFileSync(path.join('src','json','accounts.json'), 'utf-8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join('src','json','users.json'), 'utf-8');
+const users = JSON.parse(userData);
+
 app.get('/',(req, res) => 
-  res.render('index', { title: 'Index' })
+  res.render('index', { title: 'Accounts summary', accounts: accounts })
 );
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
 
